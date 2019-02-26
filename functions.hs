86544e053b2979f11f46d65bdc203b9a0d6baccf
Stephen Mahon <S.Mahon@ed,ac.uk>
2019-02-26 23:30:41 +0000

    Continuing with Learn You a Haskell for Great Good. Added new length function and some additional comments

--- functions.hs
+++ functions.hs
@@ -12,3 +12,12 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
     then x
     else doubleMe x
+
+-- boomBang replaces each odd number greater than 10 with "BANG!" and each odd number that's less than 10 with "BOOM!".
+-- If a number isn't odd, we throw it out of our list.
+boomBang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+-- _ means that we don't care what we'll draw from the list anyway so instead of writing a variable name that we'll never use,
+-- we just write _. This function replaces every element of a list with 1 and then sums that up. 
+-- This means that the resulting sum will be the length of our list.
+length' xs = sum[1 | _ <- xs]
